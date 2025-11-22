ed473845bbe806d0e6889f5ded7309356022c5b2
stevenchristian-ops <steven.christian9512@gmail.com>
2025-11-22 07:25:41 +0700

    Update pembayaran.js

--- JS/pembayaran.js
+++ JS/pembayaran.js
@@ -5,12 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const orderData = JSON.parse(localStorage.getItem("orderData"));
 
-  if (!orderData || !orderData.items || orderData.items.length === 0){
+  if (!orderData || !orderData.items){
     orderResult.innerHTML = "<p class='text-red-500 font-bold'>Tidak ada pesanan.</p>";
     return;
   }
 
-  // TAMPILKAN PESANAN
   let html = `
     <h2 class="text-xl font-bold mb-3 text-pink-600">Pesanan Kamu</h2>
     <ul>
@@ -19,7 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
   orderData.items.forEach(item => {
     html += `
       <li class="mb-4 flex gap-4 bg-pink-50 p-3 rounded-xl shadow">
-        <img src="${item.img}" class="w-20 h-20 rounded-lg border border-pink-200 object-cover">
+
+        <img src="${item.img}" class="w-20 h-20 rounded-xl border object-cover">
 
         <div>
           <p class="font-bold">${item.nama}</p>
@@ -27,23 +27,24 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>Harga: Rp ${(item.harga * item.qty).toLocaleString()}</p>
           ${item.request ? `<p class="italic text-gray-600">Request: ${item.request}</p>` : ""}
         </div>
-      </li>`;
+
+      </li>
+    `;
   });
 
   html += `
     </ul>
-    <p class="text-xl font-bold mt-3">Total: Rp ${orderData.total.toLocaleString()}</p>
+    <p class="text-xl font-bold mt-4">Total: Rp ${orderData.total.toLocaleString()}</p>
   `;
 
   orderResult.innerHTML = html;
 
-  // PROSES PEMBAYARAN
-  paymentForm.addEventListener("submit", (e) => {
+  paymentForm.addEventListener("submit", (e)=> {
     e.preventDefault();
 
-    const metode = document.querySelector('input[name="pembayaran"]:checked');
-    if (!metode){
-      alert("Pilih metode pembayaran terlebih dahulu!");
+    const metode = document.querySelector("input[name='pembayaran']:checked");
+    if(!metode){
+      alert("Pilih metode pembayaran!");
       return;
     }
 
